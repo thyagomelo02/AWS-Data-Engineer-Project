938d2ce0ef2ed6c5fc705d67d2228fac5585230a
Thyago Melo dos Santos <31568098+thyagomelo02@users.noreply.github.com>
2024-08-27 19:39:26 -0300

    Update redshift_connection.py

--- Data_Warehouse_Redshift/redshift_connection.py
+++ Data_Warehouse_Redshift/redshift_connection.py
@@ -9,7 +9,7 @@ def create_redshift_connection():
             dbname='db_olist1',
             user='olist',
             password='Olist2024',
-            host='olist-redshiftcluter.cme8c8uumlvh.us-east-1.redshift.amazonaws.com',
+            host='Cluster ARN',
             port='5439'
         )
         print("AQUIIII")
@@ -19,4 +19,3 @@ def create_redshift_connection():
         logging.error(f"Error connecting to the database: {e}")
         return None
     
-#ParameterKey=MasterUsername,ParameterValue=olist ParameterKey=MasterUserPassword,ParameterValue=Olist2024 ParameterKey=DatabaseName,ParameterValue=db_olist1 ParameterKey=ClusterType,ParameterValue=single-node ParameterKey=NumberOfNodes,ParameterValue=1 ParameterKey=S3BucketName,ParameterValue=bucket-olist-cleaned-zone ParameterKey=VpcCidr,ParameterValue=10.0.0.0/16 ParameterKey=SubnetCidr1,ParameterValue=10.0.1.0/24 ParameterKey=SubnetCidr2,ParameterValue=10.0.2.0/24 --capabilities CAPABILITY_NAMED_IAM --region us-east-1
